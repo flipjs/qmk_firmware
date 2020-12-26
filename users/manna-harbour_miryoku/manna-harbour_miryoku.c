7c0f9a32ad9268bcc94bc11bf3c28bdf700bd815
Felipe Apostol <fapostol@gogoair.com>
2020-12-26 17:18:18 -0600

    rearrange media keys bindings

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -11,8 +11,8 @@ MT(MOD_LSFT,KC_Z),  MT(MOD_LALT,KC_X),  MT(MOD_LCTL,KC_C),  MT(MOD_LGUI,KC_V),  
 KC_NP,              KC_NP,              MO(NSSL),           MO(NSL),            LT(NAVR,KC_SPC),  LT(NAVR,KC_ENT),  MT(MOD_LCTL,KC_BSPC),  KC_LGUI,               KC_NP,                KC_NP
 ),
 [NAVR] = LAYOUT_miryoku(
-KC_ESC,      KC_NA,       KC_UP,          KC_NA,         KC_NA,   KC_VOLD,       KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_VOLU,
-LGUI(KC_A),  KC_LEFT,     KC_DOWN,        KC_RIGHT,      KC_NA,   KC_LEFT,       KC_DOWN,  KC_UP,    KC_RGHT,  KC_MUTE,
+KC_ESC,      KC_NA,       KC_UP,          KC_NA,         KC_NA,   KC_NA,         KC_NA,    KC_NA,    KC_NA,    KC_NA,
+LGUI(KC_A),  KC_LEFT,     KC_DOWN,        KC_RIGHT,      KC_NA,   KC_LEFT,       KC_DOWN,  KC_UP,    KC_RGHT,  KC_NA,
 LGUI(KC_Z),  LGUI(KC_X),  LGUI(KC_C),     LGUI(KC_V),    KC_NA,   KC_MEH,        KC_LGUI,  KC_LCTL,  KC_LALT,  KC_LSFT,
 KC_NP,       KC_NP,       LSFT(KC_COMM),  LSFT(KC_DOT),  KC_SPC,  LGUI(KC_SPC),  KC_NA,    KC_NA,    KC_NP,    KC_NP
 ),
@@ -29,10 +29,10 @@ KC_LSFT,     KC_LALT,  KC_LCTL,  KC_LGUI,  LCTL(KC_N),  KC_GRV,   KC_7,     KC_8
 KC_NP,       KC_NP,    KC_NA,    KC_NA,    KC_SPC,      KC_ENT,   KC_BSPC,  KC_DEL,  KC_NP,  KC_NP
 ),
 [FUNL] = LAYOUT_miryoku(
-KC_PSCR,  KC_F1,  KC_F2,   KC_F3,    KC_F10,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  RESET,
-KC_SLCK,  KC_F4,  KC_F5,   KC_F6,    KC_F11,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  KC_NA,
-KC_PAUS,  KC_F7,  KC_F8,   KC_F9,    KC_F12,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  KC_NA,
-KC_NP,    KC_NP,  KC_INS,  KC_CAPS,  KC_NA,   KC_NA,  KC_NA,  KC_NA,  KC_NP,  KC_NP
+KC_PSCR,  KC_F1,  KC_F2,    KC_F3,    KC_F10,   KC_NA,    KC_NA,    KC_NA,    KC_NA,  KC_CAPS,
+KC_SLCK,  KC_F4,  KC_F5,    KC_F6,    KC_F11,   KC_NA,    KC_NA,    KC_NA,    KC_NA,  KC_NA,
+KC_PAUS,  KC_F7,  KC_F8,    KC_F9,    KC_F12,   KC_NA,    KC_NA,    KC_NA,    KC_NA,  RESET,
+KC_NP,    KC_NP,  KC_MPRV,  KC_MNXT,  KC_MPLY,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_NP,  KC_NP
 )
 };
 
