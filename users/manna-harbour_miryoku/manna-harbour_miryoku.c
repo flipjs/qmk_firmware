ab013a94fdad9ad089a9207578d415d012e859d4
Felipe Apostol <fapostol@gogoair.com>
2021-01-16 21:47:58 -0600

    rearrange brackets to make way for arrow keys; move media keys

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -11,27 +11,27 @@ MT(MOD_LSFT,KC_Z),  MT(MOD_LALT,KC_X),  MT(MOD_LCTL,KC_C),  MT(MOD_LGUI,KC_V),  
 KC_NP,       KC_NP,       MO(NSSL),           MO(NSL),           LT(NAVR,KC_SPC),    LT(NAVR,KC_ENT),    MT(MOD_LCTL,KC_BSPC),  KC_LSFT,    KC_NP,    KC_NP
 ),
 [NAVR] = LAYOUT_miryoku(
-KC_MUTE,        KC_VOLD,       KC_UP,       KC_VOLU,       KC_MPRV,  KC_NA,    KC_NA,    KC_NA,    KC_NA,    KC_NA,
-LGUI(KC_A),     KC_LEFT,       KC_DOWN,     KC_RIGHT,      KC_MPLY,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_NA,
-LGUI(KC_Z),     LGUI(KC_X),    LGUI(KC_C),  LGUI(KC_V),    KC_MNXT,  KC_MEH,   KC_LGUI,  KC_LCTL,  KC_LALT,  KC_LSFT,
+KC_ESC,      KC_NA,       KC_UP,          KC_NA,         LCTL(KC_P),  KC_NA,         KC_NA,    KC_NA,    KC_NA,    KC_NA,
+LGUI(KC_A),  KC_LEFT,     KC_DOWN,        KC_RGHT,       KC_TAB,      KC_LEFT,       KC_DOWN,  KC_UP,    KC_RGHT,  KC_MUTE,
+LGUI(KC_Z),  LGUI(KC_X),  LGUI(KC_C),     LGUI(KC_V),    LCTL(KC_N),  KC_VOLD,       KC_VOLU,  KC_MPRV,  KC_MNXT,  KC_MPLY,
 KC_NP,       KC_NP,       LSFT(KC_COMM),  LSFT(KC_DOT),  KC_SPC,      LGUI(KC_SPC),  KC_NA,    KC_NA,    KC_NP,    KC_NP
 ),
 [NSSL] = LAYOUT_miryoku(
-KC_ESC,      KC_NA,    KC_NA,    KC_NA,    LCTL(KC_P),  KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
-LCTL(KC_A),  KC_NA,    KC_NA,    KC_NA,    KC_TAB,      KC_UNDS,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_DQUO,
-KC_LSFT,     KC_LALT,  KC_LCTL,  KC_LGUI,  LCTL(KC_N),  KC_TILD,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,
-KC_NP,       KC_NP,       KC_NA,       KC_NA,    KC_SPC,   KC_ENT,   KC_BSPC,     KC_DEL,    KC_NP,    KC_NP
+KC_LCBR,  KC_LPRN,  KC_UP,    KC_RPRN,  KC_RCBR,     KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
+KC_LBRC,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_RBRC,     KC_UNDS,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_DQUO,
+KC_LSFT,  KC_LALT,  KC_LCTL,  KC_LGUI,  LCTL(KC_A),  KC_TILD,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,
+KC_NP,    KC_NP,    KC_NA,    KC_NA,    KC_SPC,      KC_ENT,   KC_BSPC,  KC_DEL,   KC_NP,    KC_NP
 ),
 [NSL] = LAYOUT_miryoku(
-KC_ESC,      KC_LBRC,  KC_LPRN,  KC_RPRN,  LCTL(KC_P),  KC_EQL,   KC_1,  KC_2,  KC_3,  KC_BSLS,
-LCTL(KC_A),  KC_RBRC,  KC_LCBR,  KC_RCBR,  KC_TAB,      KC_MINS,  KC_4,  KC_5,  KC_6,  KC_QUOT,
-KC_LSFT,     KC_LALT,  KC_LCTL,  KC_LGUI,  LCTL(KC_N),  KC_GRV,   KC_7,  KC_8,  KC_9,  KC_0,
-KC_NP,       KC_NP,       KC_NA,       KC_NA,    KC_SPC,   KC_ENT,   KC_BSPC,     KC_DEL,    KC_NP,    KC_NP
+KC_LCBR,  KC_LPRN,  KC_UP,    KC_RPRN,  KC_RCBR,     KC_EQL,   KC_1,     KC_2,    KC_3,   KC_BSLS,
+KC_LBRC,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_RBRC,     KC_MINS,  KC_4,     KC_5,    KC_6,   KC_QUOT,
+KC_LSFT,  KC_LALT,  KC_LCTL,  KC_LGUI,  LCTL(KC_A),  KC_GRV,   KC_7,     KC_8,    KC_9,   KC_0,
+KC_NP,    KC_NP,    KC_NA,    KC_NA,    KC_SPC,      KC_ENT,   KC_BSPC,  KC_DOT,  KC_NP,  KC_NP
 ),
 [FUNL] = LAYOUT_miryoku(
-KC_PSCR,  KC_F1,    KC_F2,  KC_F3,  KC_F10,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  RESET,
-KC_SLCK,  KC_F4,    KC_F5,  KC_F6,  KC_F11,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  KC_NA,
-KC_PAUS,  KC_F7,    KC_F8,  KC_F9,  KC_F12,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  KC_NA,
-KC_NP,       KC_NP,       KC_INS,   KC_CAPS,  KC_NA,  KC_NA,  KC_NA,   KC_NA,    KC_NP,    KC_NP
+KC_PSCR,  KC_F1,  KC_F2,   KC_F3,    KC_F10,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  RESET,
+KC_SLCK,  KC_F4,  KC_F5,   KC_F6,    KC_F11,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  KC_NA,
+KC_PAUS,  KC_F7,  KC_F8,   KC_F9,    KC_F12,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  KC_NA,
+KC_NP,    KC_NP,  KC_INS,  KC_CAPS,  KC_NA,   KC_NA,  KC_NA,  KC_NA,  KC_NP,  KC_NP
 )
 };
