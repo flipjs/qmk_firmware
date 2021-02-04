86c713d67287bfd47cf07b608c0f3cd232df8935
Felipe Apostol <fapostol@gogoair.com>
2021-02-03 21:07:02 -0600

    rename layers to be the same names use in gergoplex

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -6,8 +6,6 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case MT(MOD_LCTL,KC_C):
             return TAPPING_TERM + 20;
-        case MT(MOD_LGUI,KC_V):
-            return TAPPING_TERM - 10;
         case MT(MOD_LSFT,KC_Z):
         case MT(MOD_LSFT,KC_SLSH):
             return TAPPING_TERM - 20;
@@ -20,29 +18,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_miryoku(
 KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,             KC_Y,             KC_U,                  KC_I,                  KC_O,                 KC_P,
-KC_A,               KC_S,               KC_D,               KC_F,               KC_G,             KC_H,             KC_J,                  KC_K,                  KC_L,                 LT(FUNL,KC_SCLN),
+KC_A,               KC_S,               KC_D,               KC_F,               KC_G,             KC_H,             KC_J,                  KC_K,                  KC_L,                 LT(FUN,KC_SCLN),
 MT(MOD_LSFT,KC_Z),  MT(MOD_LALT,KC_X),  MT(MOD_LCTL,KC_C),  MT(MOD_LGUI,KC_V),  MEH_T(KC_B),      MEH_T(KC_N),      MT(MOD_LGUI,KC_M),     MT(MOD_LCTL,KC_COMM),  MT(MOD_LALT,KC_DOT),  MT(MOD_LSFT,KC_SLSH),
-KC_NP,              KC_NP,              MO(NSSL),           MO(NSL),            LT(NAVR,KC_SPC),  LT(NAVR,KC_ENT),  MT(MOD_LCTL,KC_BSPC),  KC_LGUI,               KC_NP,                KC_NP
+KC_NP,              KC_NP,              MO(SYMB),           MO(NUMB),            LT(NAVI,KC_SPC),  LT(NAVI,KC_ENT),  MT(MOD_LCTL,KC_BSPC),  KC_LGUI,               KC_NP,                KC_NP
 ),
-[NAVR] = LAYOUT_miryoku(
+[NAVI] = LAYOUT_miryoku(
 KC_ESC,      KC_NA,       KC_UP,          KC_NA,         LCTL(KC_P),  KC_NA,         KC_NA,    KC_NA,    KC_NA,    KC_NA,
 LGUI(KC_A),  KC_LEFT,     KC_DOWN,        KC_RGHT,       KC_TAB,      KC_LEFT,       KC_DOWN,  KC_UP,    KC_RGHT,  KC_MUTE,
 LGUI(KC_Z),  LGUI(KC_X),  LGUI(KC_C),     LGUI(KC_V),    LCTL(KC_N),  KC_VOLD,       KC_VOLU,  KC_MPRV,  KC_MNXT,  KC_MPLY,
 KC_NP,       KC_NP,       LSFT(KC_COMM),  LSFT(KC_DOT),  KC_SPC,      LGUI(KC_SPC),  KC_NA,    KC_NA,    KC_NP,    KC_NP
 ),
-[NSSL] = LAYOUT_miryoku(
+[SYMB] = LAYOUT_miryoku(
 KC_NA,       KC_LBRC,  KC_LPRN,  KC_RPRN,  KC_NA,   KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
 LCTL(KC_A),  KC_RBRC,  KC_LCBR,  KC_RCBR,  KC_NA,   KC_UNDS,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_DQUO,
 KC_LSFT,     KC_LALT,  KC_LCTL,  KC_LGUI,  KC_NA,   KC_TILD,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,
 KC_NP,       KC_NP,    KC_NA,    KC_NA,    KC_SPC,  KC_ENT,   KC_BSPC,  KC_DEL,   KC_NP,    KC_NP
 ),
-[NSL] = LAYOUT_miryoku(
+[NUMB] = LAYOUT_miryoku(
 KC_NA,       KC_LBRC,  KC_LPRN,  KC_RPRN,  KC_NA,   KC_EQL,   KC_1,     KC_2,    KC_3,   KC_BSLS,
 LCTL(KC_A),  KC_RBRC,  KC_LCBR,  KC_RCBR,  KC_NA,   KC_MINS,  KC_4,     KC_5,    KC_6,   KC_QUOT,
 KC_LSFT,     KC_LALT,  KC_LCTL,  KC_LGUI,  KC_NA,   KC_GRV,   KC_7,     KC_8,    KC_9,   KC_0,
 KC_NP,       KC_NP,    KC_NA,    KC_NA,    KC_SPC,  KC_ENT,   KC_BSPC,  KC_DOT,  KC_NP,  KC_NP
 ),
-[FUNL] = LAYOUT_miryoku(
+[FUN] = LAYOUT_miryoku(
 KC_PSCR,  KC_F1,  KC_F2,   KC_F3,    KC_F10,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  RESET,
 KC_SLCK,  KC_F4,  KC_F5,   KC_F6,    KC_F11,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  KC_NA,
 KC_PAUS,  KC_F7,  KC_F8,   KC_F9,    KC_F12,  KC_NA,  KC_NA,  KC_NA,  KC_NA,  KC_NA,
